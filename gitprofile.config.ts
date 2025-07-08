5bfc74f50c5660e7e58dc20839044e86260a0f2d
Puran Paodensakul <puranpoudelzack@gmail.com>
2025-07-08 23:46:41 +0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -80,7 +80,7 @@ const CONFIG = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://github.com/zackpdl/zackpdl.github.io/blob/main/resume.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
    // 'PHP',
